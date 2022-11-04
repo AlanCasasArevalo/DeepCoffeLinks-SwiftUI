2188c5a623abf0cda92a93d8be87454795ca675a
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-04 16:12:49 +0100

    Check if deep link cames from real url app or not

--- DeepCoffeLinks/DeepCoffeLinksApp.swift
+++ DeepCoffeLinks/DeepCoffeLinksApp.swift
@@ -9,7 +9,13 @@ struct DeepCoffeLinksApp: App {
             ContentView()
                 .environmentObject(appData)
                 .onOpenURL { url in
-                    print(url)
+                    if appData.checkDeepLink(url: url) {
+                        print(url)
+                        print("FROM Deep Link")
+                    } else {
+                        print(url)
+                        print("FALL BACK Deep Link")
+                    }
                 }
         }
     }
